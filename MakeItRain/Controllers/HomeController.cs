6d02151f3f5ac57376769e80296b9aedf49c8651
cjl1750 <cjl1750@rit.edu>
2014-03-05 17:47:05 -0500

    Added some facebook javascript shenanigans

--- MakeItRain/Controllers/HomeController.cs
+++ MakeItRain/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,8 +10,16 @@ namespace MakeItRain.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
+            var client = new Facebook.FacebookClient();
+
+
+            if (client == null)  //This needs to check if the user is "logged in"
+            {
+                return Redirect("./account/login");
+            }
             return View();
         }
 
