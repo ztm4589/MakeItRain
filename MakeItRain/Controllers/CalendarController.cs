891df32e9968d038dd8b828a7464e2349cdac66c
Zach Masiello <ztm4589@rit.edu>
2014-05-02 12:24:40 -0400

    stuffs

---
+++ MakeItRain/Controllers/CalendarController.cs
@@ -0,0 +1,25 @@
+﻿using MakeItRain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MakeItRain.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Backend/
+        //public ActionResult Backend()
+        //{
+            //return new Dpc().CallBack(this);
+        //}
+    }
+}
